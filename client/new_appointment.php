85ee03653f8d4cef6413810e4cb9aeb046672659
jolorambongga <151989553+jolorambongga@users.noreply.github.com>
2024-06-26 09:15:55 +0800

    Add files via upload

--- client/new_appointment.php
+++ client/new_appointment.php
@@ -9,9 +9,8 @@ include_once('handles/auth.php');
 checkAuth();
 ?>
 
-<!-- date picker -->
+<!-- Datepicker CSS -->
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datepicker/1.9.0/css/bootstrap-datepicker.min.css">
-<script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datepicker/1.9.0/js/bootstrap-datepicker.min.js"></script>
 
 <div class="my-wrapper">
     <div class="container-fluid">
@@ -20,7 +19,7 @@ checkAuth();
                 <h1 class="text-start">Make your new appointment</h1>
             </div>
         </div>
-        <!-- start multi-step form -->
+        <!-- Start multi-step form -->
         <div class="row justify-content-center bg- p-jp-md-5">
             <div class="col-12 col-md-8 col-lg-6">
                 <div class="wrapper mt-5">
@@ -31,6 +30,7 @@ checkAuth();
                             <div class="box mb-3">
                                 <select id="procedure-select" class="form-control">
                                     <!-- Options will be loaded here by jQuery -->
+                                    <option value="" disabled selected>Select a procedure</option>
                                 </select>
                             </div>
                             <button type="button" class="btn btn-warning next-btn float-end mt-3">Next</button>
@@ -40,7 +40,7 @@ checkAuth();
                         <div id="step-2" class="form-step" style="display:none;">
                             <div class="title">Upload Photo of Your Request</div>
                             <div class="box mb-3">
-                                <input accept="image/jpeg, image/png, image/gif" type="file" name="request_image" id="request_image" class="form-control">              
+                                <input accept="image/jpeg, image/png, image/gif" type="file" name="request_image" id="request_image" class="form-control">
                             </div>
                             <button type="button" class="btn btn-warning next-btn float-end mt-3">Next</button>
                             <button type="button" class="btn btn-danger prev-btn float-end mt-3 me-2">Previous</button>
@@ -52,17 +52,12 @@ checkAuth();
                             <div class="box mb-3">
                                 <label for="appointment_date">Select Appointment Date:</label>
                                 <input type="text" id="appointment_date" name="appointment_date" class="form-control">
-                                <pre></pre>
 
-                                <select class="form-select" aria-label="Default select example">
-                                    <option value disabled selected>Select Time</option>
-                                    <option value="1">One</option>
-                                    <option value="2">Two</option>
-                                    <option value="3">Three</option>
+                                <select id="appointment_time" class="form-select mt-3" aria-label="Default select example">
+                                    <option value="" disabled selected>Select Time</option>
+                                    <!-- Times will be populated dynamically -->
                                 </select>
-
                             </div>
-
                             <button type="button" class="btn btn-warning next-btn float-end mt-3">Next</button>
                             <button type="button" class="btn btn-danger prev-btn float-end mt-3 me-2">Previous</button>
                         </div>
@@ -80,19 +75,26 @@ checkAuth();
                 </div>
             </div>
         </div>
-        <!-- end multi-step form -->
-        <div id="load_spinner" class="d-flex justify-content-center" style="display: none;">
+        <!-- End multi-step form -->
 
+        <!-- Spinner for loading -->
+        <div id="load_spinner" class="d-flex justify-content-center" style="display: none;">
+            <!-- Your spinner HTML or loading message here -->
         </div>
 
+        <!-- Schedule information -->
         <input type="hidden" id="schedule" name="schedule"/>
         <div id="schedule_body"></div>
     </div>
 </div>
 
+<!-- jQuery and Bootstrap Datepicker JS -->
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datepicker/1.9.0/js/bootstrap-datepicker.min.js"></script>
 
 <script>
 $(document).ready(function () {
+
     var schedule = []; // Define the schedule array globally
 
     // Function to fetch and save schedule data
@@ -117,17 +119,18 @@ $(document).ready(function () {
                     schedule.push(scheduleItem); // Push schedule item to array
 
                     const schedule_html = `
-                    <div>
-                    <p>DAY OF WEEK: ${data.day_of_week}</p>
-                    <p>START TIME: ${data.start_time}</p>
-                    <p>END TIME: ${data.end_time}</p>
-                    </div>
-                    <hr>
+                        <div>
+                            <p>DAY OF WEEK: ${data.day_of_week}</p>
+                            <p>START TIME: ${data.start_time}</p>
+                            <p>END TIME: ${data.end_time}</p>
+                        </div>
+                        <hr>
                     `;
                     body.append(schedule_html); // Append schedule HTML to body
                 });
 
                 console.log("Schedule List:", schedule);
+                schedule = schedule.filter(Boolean);
                 updateDatePickerAvailability(); // Call function to update datepicker
             },
             error: function(error) {
@@ -138,28 +141,22 @@ $(document).ready(function () {
 
     // Function to update datepicker availability based on schedule
     function updateDatePickerAvailability() {
-        $("#appointment_date").datepicker("destroy"); // Destroy existing datepicker instance
-        
         // Array to map numeric day index to string day name
         var dayNames = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
 
-        $("#appointment_date").datepicker({
+        $("#appointment_date").datepicker("option", {
             dateFormat: 'yy-mm-dd',
             showButtonPanel: true,
             minDate: 0,
             changeMonth: true,
-            changeYear: true,
+            changeYear: false,
             beforeShowDay: function(date) {
-                var day = date.getDay(); // Get day of the week (0 - Sunday, 1 - Monday, ..., 6 - Saturday)
-
-                // Check if day is enabled based on schedule
+                var day = date.getDay();
+                var dayName = dayNames[day];
                 var isEnabled = schedule.some(function(item) {
-                    // Use the day index directly for comparison
-                    var scheduleDay = dayNames.indexOf(item.day_of_week);
-                    return scheduleDay === day;
+                    return item.day_of_week === dayName;
                 });
-
-                return [isEnabled, isEnabled ? '' : 'disabled']; // Return [true/false, 'custom-css-class'] based on isEnabled
+                return [isEnabled, isEnabled ? "" : "disabled"];
             },
             onSelect: function(dateText) {
                 // Implement any additional logic on date selection if needed
@@ -193,15 +190,6 @@ $(document).ready(function () {
                     const option = `<option value="${value.service_id}" data-service-name="${value.service_name}" data-service-duration="${value.duration}" data-service-max="${value.max}" data-doctor-id="${value.doctor_id}">${value.service_name}</option>`;
                     $('#procedure-select').append(option);
                 });
-
-                // Initialize datepicker after loading procedures
-                $("#appointment_date").datepicker({
-                    dateFormat: 'yy-mm-dd',
-                    showButtonPanel: true,
-                    minDate: 0,
-                    changeMonth: true,
-                    changeYear: false
-                });
             },
             error: function(error) {
                 console.log("Error loading procedures:", error);
@@ -223,11 +211,11 @@ $(document).ready(function () {
                 return;
             }
         } else if ($currentStep.attr('id') === 'step-3') {
-            if ($('#appointment_date').val() === '' || $('.form-select').val() === null) {
+            if ($('#appointment_date').val() === '' || $('#appointment_time').val() === null) {
                 alert('Please select a date and time before proceeding.');
                 return;
             }
-            var appointmentTime = new Date($('#appointment_date').val() + 'T' + $('.form-select').val());
+            var appointmentTime = new Date($('#appointment_date').val() + 'T' + $('#appointment_time').val());
             var now = new Date();
             now.setHours(now.getHours() + 1);
 
@@ -251,11 +239,10 @@ $(document).ready(function () {
 
     // Function to populate review box content
     function populateReviewBox() {
-        var service_id = $('#procedure-select').val();
         var service_name = $('#procedure-select option:selected').data('service-name');
         var request_image = $('#request_image')[0].files[0];
         var appointment_date = $('#appointment_date').val();
-        var appointment_time = $('.form-select').val();
+        var appointment_time = $('#appointment_time').val();
 
         $('#review-box').html(`
             <p><strong>Procedure:</strong> ${service_name}</p>
@@ -272,7 +259,7 @@ $(document).ready(function () {
         var service_name = $('#procedure-select option:selected').data('service-name');
         var request_image = $('#request_image')[0].files[0];
         var appointment_date = $('#appointment_date').val();
-        var appointment_time = $('.form-select').val();
+        var appointment_time = $('#appointment_time').val();
         var formData = new FormData();
         formData.append('user_id', <?php echo($_SESSION['user_id']); ?>);
         formData.append('full_name', "<?php echo $_SESSION['first_name'] . ' ' . $_SESSION['last_name']; ?>");
@@ -303,9 +290,27 @@ $(document).ready(function () {
             }
         });
     });
+
+    // Initialize datepicker after loading procedures
+    $(document).ajaxComplete(function() {
+        $("#appointment_date").datepicker({
+            dateFormat: 'yy-mm-dd',
+            showButtonPanel: true,
+            minDate: 0,
+            changeMonth: true,
+            changeYear: false
+        });
+    });
+
+    // Event handler for updating appointment time options based on date selection
+    $(document).on('change', '#appointment_date', function() {
+        var selectedDate = $(this).val();
+        // Implement logic to fetch and populate appointment times dynamically
+        // Example: AJAX call to fetch available times and populate #appointment_time select options
+    });
+
 });
 </script>
-
 
 <?php
 include_once('footer_script.php');
